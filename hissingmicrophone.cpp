f1b050999913ab5f79316159fa4a863af42be11d
kykorman <kykorman@gmail.com>
2018-04-23 18:56:08 -0500

    Realized that I missed one file, removed some tabs

--- hissingmicrophone.cpp
+++ hissingmicrophone.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int main(){
-        string word;
-        cin>>word;
-        for(int i=0;i<word.length();i++){
-                if(word[i]=='s'&&word[i+1]=='s'){
-                        cout<<"hiss"<<endl;
-                        return 0;
-                }
-        }
+	string word;
+	cin>>word;
+	for(int i=0;i<word.length();i++){
+		if(word[i]=='s'&&word[i+1]=='s'){
+			cout<<"hiss"<<endl;
+			return 0;
+		}
+	}
 cout<<"no hiss"<<endl;
 return 0;}
---
+++ twostones.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+        int N;
+        scanf("%i", &N);
+
+        if (N % 2 == 1)
+                puts("Alice");
+        else
+                puts("Bob");
+
+return 0;}
